fcfe957644e76d1eb4d7d20b718fc913c81caf11
Lee <harpergly@gmail.com>
2026-03-06 19:02:27 -0500

    new load method

---
+++ js/components.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  const components = document.querySelectorAll("[data-component]");
+
+  for (const element of components) {
+    const name = element.dataset.component;
+    const path = `components/${name}.html`;
+
+    try {
+      const response = await fetch(path);
+      const html = await response.text();
+
+      element.innerHTML = html;
+    } catch (error) {
+      console.error(`Component "${name}" failed to load.`);
+    }
+  }
+});
--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 const toggle = document.getElementById("darkModeToggle");
 const body = document.body;
 
-
 if (localStorage.getItem("darkMode") === "enabled") {
-    body.classList.add("dark-Mode");
+  body.classList.add("dark-Mode");
 }
 
 toggle.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
+  body.classList.toggle("dark-mode");
 
-    // Save preference
-    if (body.classList.contains("dark-mode")) {
-        localStorage.setItem("darkMode", "enabled");
-    } else {
-        localStorage.setItem("darkMode", "disabled");
-    }
+  // Save preference
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    localStorage.setItem("darkMode", "disabled");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("darkModeToggle");
+
+  if (toggle) {
+    toggle.addEventListener("click", () => {
+      document.body.classList.toggle("dark-mode");
+    });
+  }
 });
